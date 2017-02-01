b6cbefdc4fdad7dd3a720229c0befa8d281d8aa9
jeremy louis <jeremy.louis@cegetel.net>
2017-02-01 16:15:06 +0100

    Mise en place des 4 tests, 1 pour chaque fonction

--- tests/Lib/stringFormatterTest.php
+++ tests/Lib/stringFormatterTest.php
@@ -6,7 +6,19 @@ use Lib\stringFormatter;
 
 class stringFormatterTest extends \PHPUnit_Framework_TestCase{
 	
-	public function testDuTest(){
+	public function testPrefix(){
+		echo 'message_test';
+	}
+
+	public function testSuffix(){
+		echo 'message_test';
+	}
+
+	public function testToCamelCase(){
+		echo 'message_test';
+	}
+
+	public function testConcatString(){
 		echo 'message_test';
 	}
 }
