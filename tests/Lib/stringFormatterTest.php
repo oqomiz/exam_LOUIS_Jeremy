68a49c73dba39050152c370119965470597c8368
jeremy louis <jeremy.louis@cegetel.net>
2017-02-01 16:43:41 +0100

    ajout de commentaires pour une meilleure compréhension!

--- tests/Lib/stringFormatterTest.php
+++ tests/Lib/stringFormatterTest.php
@@ -6,45 +6,88 @@ use Lib\stringFormatter;
 
 class stringFormatterTest extends \PHPUnit_Framework_TestCase{
 	
+	
+	// Tests de prefix
+
 	public function testPrefix(){
+
+		//Initialise un nouvel objet StringFormatter
 		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction prefix avec les arguments 'abc' et 'def'
 		$result = $stringFormatter->prefix('abc','def');
 
-		$this->assertSame($result,'abcdef');
-	}
-
-	public function testSuffix(){
-		$stringFormatter = new StringFormatter();
-		$result = $stringFormatter->suffix('abc','def');
-
-		$this->assertSame($result,'defabc');
-	}
-
-	public function testToCamelCase(){
-		$stringFormatter = new StringFormatter();
-		$result = $stringFormatter->toCamelCase('abc','def');
-
-		$this->assertSame($result,'abcDef');
-	}
-
-	public function testConcatString(){
-		$stringFormatter = new StringFormatter();
-		$result = $stringFormatter->concatString('abc','def');
-
+		//Vérification du résultat
 		$this->assertSame($result,'abcdef');
 	}
 
 	public function testPrefixFull(){
+
+		//Initialise un nouvel objet StringFormatter
 		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction prefix avec les arguments 'abc','def' et true
 		$result = $stringFormatter->prefix('abc','def',true);
 
+		//Vérification du résultat
 		$this->assertSame($result,'abcDef');
 	}
 
-	public function testSuffixFull(){
+
+	//Tests de suffix
+
+	public function testSuffix(){
+
+		//initialise un nouvel objet StringFormatter
 		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction suffix avec les arguments 'abc' et 'def'
+		$result = $stringFormatter->suffix('abc','def');
+
+		//Vérification du résultat
+		$this->assertSame($result,'defabc');
+	}
+
+	public function testSuffixFull(){
+
+		//initialise un nouvel objet StringFormatter
+		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction suffix avec les arguments 'abc','def' et true
 		$result = $stringFormatter->suffix('abc','def',true);
 
+		//Vérification du résultat
 		$this->assertSame($result,'defAbc');
 	}
+
+
+	//Test de toCamelCase
+
+	public function testToCamelCase(){
+
+		//initialise un nouvel objet StringFormatter
+		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction toCamelCase avec les arguments 'abc' et 'def'
+		$result = $stringFormatter->toCamelCase('abc','def');
+
+		//Vérification du résultat
+		$this->assertSame($result,'abcDef');
+	}
+
+
+	//Test de concatString
+
+	public function testConcatString(){
+
+		//initialise un nouvel objet StringFormatter
+		$stringFormatter = new StringFormatter();
+
+		//Éxécution de la fonction concatString avec les arguments 'abc' et 'def'
+		$result = $stringFormatter->concatString('abc','def');
+
+		//Vérification du résultat
+		$this->assertSame($result,'abcdef');
+	}
+
 }
